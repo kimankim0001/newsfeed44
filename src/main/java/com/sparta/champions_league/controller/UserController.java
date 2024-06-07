5aac43fc775ca110fc7e18abea44b115d1c6f441
erictslee <ts5452@naver.com>
2024-06-07 15:20:48 +0900

    UserController, HTML관련 파일 삭제.

--- src/main/java/com/sparta/champions_league/controller/UserController.java
+++ src/main/java/com/sparta/champions_league/controller/UserController.java
@@ -12,26 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class UserController {
 
-    private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
-    @GetMapping("/user/login-page")
-    public String loginPage() {
-        return "login";
-    }
-
-    @GetMapping("/user/signup")
-    public String signupPage() {
-        return "signup";
-    }
-
-    @PostMapping("/user/signup")
-    public String singup(SignupRequestDto requestDto) {
-        userService.signup(requestDto);
-
-        return "redirect:/api/user/login-page";
-    }
 }
